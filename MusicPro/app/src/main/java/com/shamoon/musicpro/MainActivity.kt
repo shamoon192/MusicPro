757e63189445aa32206736c923bf63729bd9f083
Shamoon Ahmed <shamoonahmed1992@gmail.com>
2020-08-24 11:15:04 +0100

    first empty project starting commit

---
+++ MusicPro/app/src/main/java/com/shamoon/musicpro/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.shamoon.musicpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shamoon.musicpro.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
